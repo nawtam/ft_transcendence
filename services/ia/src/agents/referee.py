f51acd96fa9b301949dc25d74667258faaee2825
Elbaaaaaa <bachirbella08@gmail.com>
2026-07-31 21:21:32 +0200

    added attack() tool to agent B, it needs to be perfected.

--- services/ia/src/agents/referee.py
+++ services/ia/src/agents/referee.py
@@ -4,13 +4,31 @@ from langchain_groq import ChatGroq
 from langchain_core.messages import HumanMessage, SystemMessage
 from langchain_core.tools import tool
 
-def call_referee(State: State):
-	
-	return {"messages": [ia_response]}
-
+def get_referee_llm() -> ChatGroq:
+    return ChatGroq(
+        model="llama-3.3-70b-versatile",
+        temperature=0,
+        api_key=os.environ["GROQ_API_KEY"],
+)
 
 @tool
-def	attaquer(cible: str):
-	"""Utilise cet outil quand le joueur veut attaquer un ennemi."""
-	# PLUS TARD FAUDRA METTRE LE CODE QUI TOUCHE A LA DB ICI.
-	return f"L'attaque sur {cible} est enregistree."
+def	attack_enemy(target: str, weapon: str) -> str:
+	"""
+	Executes a physical attack sequence.
+
+	Args:
+		target_name (str): The exact name of the enemy character to hit.
+		weapon_used (str): The name of the weapon the player is using from their inventory.
+
+	Requirements:
+		Only use this tool if the user's itent is clearly offensive.
+		If the target_name is not provided, do not guess; ask for clarification.
+	"""
+	return f"Technical result: Attack initiated on {target} with {weapon}"
+
+
+
+def call_referee(State: State):
+	tools = [attack_enemy]
+	system_prompt = SystemMessage(content="Your technical instructions here...")
+	return {"messages": [ia_response]}
