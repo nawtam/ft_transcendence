70f71e413bb293e767c180e0168ec741314eb496
nawtam <nawel.tamacha31@gmail.com>
2026-08-13 16:21:47 +0200

    add universContext file : useParams (react)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,6 @@
 import { Routes, Route } from 'react-router-dom'
+import { useParams } from "react-router-dom";
+
 import Home from './pages/Home'
 import Connexion from './pages/Connexion'
 import Registration from './pages/Registration'
--- frontend/src/context/multi/universeContext.tsx
+++ frontend/src/context/multi/universeContext.tsx
@@ -1,1 +1,12 @@
-import { createContext, useContext, useState, ReactNode } from "react";
+import { useParams } from "react-router-dom";
+import { universes, type UniverseId } from './universe';
+
+export function useUniverse() {
+  const { universeId } = useParams<{ universeId: UniverseId }>();
+
+  if (!universeId) {
+    return undefined;
+  }
+
+  return universes[universeId];
+}
